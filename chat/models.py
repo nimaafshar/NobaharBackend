957b3def8c76cbbb7cfcf5b167a9c962affb2b34
nimaafshar <nima.afsharh@gmail.com>
2022-03-11 12:32:45 +0330

    message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Message:
+    message = models.CharField(500, blank=False, null=False)
+    sent_by = models.ForeignKey('account.User', on_delete=models.DO_NOTHING, null=False)
+    sent_to = models.ForeignKey('account.User', on_delete=models.DO_NOTHING, null=False)
+    date = models.DateTimeField(auto_now_add=True)
