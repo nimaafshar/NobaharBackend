5bb6f2c961e012e6524ea67ccf230c944e221c7b
nimaafshar <nima.afsharh@gmail.com>
2022-03-11 12:19:51 +0330

    account models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField('account.User', on_delete=models.PROTECT, related_name='owned_group', null=False)
+    name = models.CharField(max_length=300, blank=False, null=False)
+    description = models.TextField(null=False, blank=True)
+    connected_to = models.ManyToManyField('account.Group')
+
 
 # Create your models here.
+class User(AbstractUser):
+    """
+    todo : use email as sign in key
+    """
+
+    email = models.EmailField(_('email address'), blank=False, null=False, unique=True)
+    name = models.CharField(max_length=200, blank=False, null=False, unique=False)
+    group = models.ForeignKey('account.Group', on_delete=models.PROTECT, related_name='members', null=True)
+
+
+class JoinRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    group = models.ForeignKey('account.Group', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class ConnectionRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    from_group = models.ForeignKey('account.Group', on_delete=models.CASCADE)
+    to_group = models.ForeignKey('account.Group', on_delete=models.CASCADE)
+    sent = models.DateTimeField(auto_now_add=True)
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.User'
