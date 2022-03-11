da8781e53cf26fa467ac2c76746e804f9979873e
als1379 <a.saleh.m.a@gmail.com>
2022-03-11 15:08:40 +0330

    sign up view and url

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from .jwt import CustomTokenObtainPairSerializer
 
-# Create your views here.
+
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        print(user)
+        return Response({
+            "token": str(CustomTokenObtainPairSerializer.get_token(user)),
+            "message": "successful"
+        })
